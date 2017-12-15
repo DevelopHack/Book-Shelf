1a56d08a6475465fa4d4e2a1b66ad617813ac598
DevelopHack <lionel2688@gmail.com>
2017-12-15 13:30:05 -0600

    refactor: refactoring code
    Change in the SearchBook file to get better performance when the user make a search.

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import debounce from 'lodash.debounce'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
 class SearchBook extends Component {
-    constructor(){
-        super();
-        this.state = { query: '' }
+    constructor(props){
+        super(props);
+        this.state = { query: props.value }
 
         this.updateQuery = this.updateQuery.bind(this)
+        this.updateQuery = debounce(this.updateQuery,1000)
     }   
     /**
      * @description Parameter validation
@@ -23,7 +25,7 @@ class SearchBook extends Component {
      */
     updateQuery = (query) => { 
         this.setState({ query: query.trim() })
-    }
+    }    
     /**
      * @description Invoke immediately after updating occurs
      * @param {object} prevProps 
@@ -32,12 +34,11 @@ class SearchBook extends Component {
     componentDidUpdate(prevProps, prevState){   
         if(prevState.query !== this.state.query)
         {
-            this.props.onSearch(this.state.query)
+           this.props.onSearch(this.state.query)
         }
     }
     render(){
-        const {onUpdateBook, foundBooks}=this.props
-        const {query}=this.state           
+        const {onUpdateBook, foundBooks}=this.props                  
         return( 
             <div className="search-books">            
             <div className="search-books-bar">
@@ -55,14 +56,14 @@ class SearchBook extends Component {
 
                     However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                     you don't find a specific author or title. Every search is limited by search terms.
-                    */}                   
+                    */} 
+                                
                     <input 
                         type="text" 
                         placeholder="Search by title or author"
-                        value={query}
-                        onChange={(event) => this.updateQuery(event.target.value)}                                                     
-                    />
-                    
+                        value={this.props.value}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />                    
                 </div>
             </div>
             <div className="search-books-results">
